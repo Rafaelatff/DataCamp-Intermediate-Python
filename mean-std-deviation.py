0666f42a89bce6d4f0fdb75ee2e92496ba98e48a
Rafaela Freitas <58916022+Rafaelatff@users.noreply.github.com>
2023-07-14 21:48:02 -0300

    Finished applying X formula

--- mean-std-deviation.py
+++ mean-std-deviation.py
@@ -2,23 +2,14 @@ import numpy as np  # To use array of numbers instead of list
 import matplotlib.pyplot as plt # To plot graphics
 import random # To generate random numbers
 from statistics import mean # To use mean from statistics package
-import math # To use log methods,
+import math # To use log and pi methods.
+from scipy.stats import norm # To show gaussian curve # pip3 install scipy   
 
 ## Generating and plotting random values with uniform distribution
 
 values = np.random.randint(-10,10,1000) # Lower limit, Upper limit, number of values
 print('values is: ' + str(type(values))) # Print the type for 'values' : <class 'numpy.ndarray'>
 print(values) # Print the values for 'values' variable
-
-plt.subplot(2,2,1) # plt.subplot(nrows, ncols, index)
-plt.plot(values) # Prepare the plot to show the randon values
-plt.xlabel('Time domain') 
-plt.ylabel('Amplitude') 
-plt.title('Random values, uniform distribution')
-
-plt.subplot(222) # Also works, even whitout the coma
-plt.hist(values, bins=20)
-plt.title('Histogram')
 
 print('Mean: ' + str(np.float64(np.mean(values)))) # It returns a <numpy.float64> type, then is converted to string
 print('Standard deviation: ' + str(np.float64(np.std(values)))) # Same as before
@@ -42,23 +33,70 @@ R2 = np.array(R2)
 
 print('R1 is: ' + str(type(R1))) # It returns <class 'numpy.ndarray'>
 
-#X = np.array(2 * math.pi() * R2)
-#print(type(X))
-#X = math.log(R1)
+# Calculate logarithm for each element in the array
+log_R1 = [math.log(x) for x in R1]
 
-plt.subplot(2,2,3) # 
-plt.plot(R1) # Prepare the plot to show the randon values
+plt.subplot(4,2,1) # plt.subplot(nrows, ncols, index)
+plt.plot(log_R1) # Prepare the plot to show the randon values
+plt.ylabel('Amplitude') 
+plt.title('log_R1')
+
+plt.subplot(422) # Also works, even whitout the coma
+plt.hist(log_R1, bins=20)
+plt.title('Histogram')
+
+log_R1 = np.array(log_R1)
+log_R1 = log_R1 * -2 # To multiply, must be np.array instead of list
+sqrtM2log_R1 = np.sqrt(log_R1) # Then I do the square root of the value (same as ^1/2)
+
+print('log_R1 * -2 is: ' + str(type(log_R1))) # It returns <numpy.ndarray>
+print(log_R1)
+
+plt.subplot(4,2,3) 
+plt.plot(sqrtM2log_R1) 
+plt.ylabel('Amplitude') 
+plt.title('(-2logR1)^1/2') # First part of the equation
+
+plt.subplot(4,2,4) 
+plt.hist(sqrtM2log_R1, bins=20)
+
+cos2piR2 = 2 * math.pi * R2
+cos2piR2 = np.cos(cos2piR2)
+
+plt.subplot(4,2,5) 
+plt.plot(cos2piR2) 
+plt.ylabel('Amplitude') 
+plt.title('cos2piR2') # Second part of the equation
+
+plt.subplot(4,2,6) 
+plt.hist(cos2piR2, bins=20)
+
+X = sqrtM2log_R1 * cos2piR2
+
+plt.subplot(4,2,7) 
+plt.plot(X) # Prepare the plot to show the randon values
 plt.xlabel('Time domain') 
 plt.ylabel('Amplitude') 
-plt.title('Random values, normal distribution')
+plt.title('X')#'Random values, normal distribution')
 
-plt.subplot(2,2,4) # Shows plots in 1 = 1 row, 2 = two column, 1 = plot 1
-plt.hist(R1, bins=20)
-plt.title('Histogram')
+plt.subplot(4,2,8)
+plt.hist(X, bins=20)
+
+print('Mean: ' + str(np.float64(np.mean(X)))) # It returns a <numpy.float64> type, then is converted to string
+print('Standard deviation: ' + str(np.float64(np.std(X)))) # Same as before
+
+
+x = np.linspace(np.mean(X) - 4 * np.std(X), np.mean(X) + 4 * np.std(X), 100)  # Valores de x para a distribuição gaussiana
+y = norm.pdf(x, np.mean(X), np.std(X))
+
+plt.subplot(4,2,8)
+plt.twinx()
+plt.plot(x, y, color='red', linewidth=2)
 
 plt.show()
 
-
+# X Mean: 0.003880677981061158
+# X Standard deviation: 1.017240401517945
 
 
 
